2ba39b5a960999b571cd693e11429c4f194f43cf
jiwothur <jiwothur@naver.com>
2023-02-13 12:31:24 +0900

    Add ChartBar

--- my-react-app/src/components/NewExpense/ExpenseForm.js
+++ my-react-app/src/components/NewExpense/ExpenseForm.js
@@ -4,7 +4,6 @@ import './ExpenseForm.css';
 * @author
 * @function ExpenseForm
 **/
-//test2
 const ExpenseForm = (props) => {
   const [enteredTitle,setEnteredTitle] = useState(''); // 컴포넌트 함수의 수명 주기와는 별개인 변수에 저장
   const [enteredAmount,setEnteredAmount] = useState('');
@@ -43,23 +42,40 @@ const ExpenseForm = (props) => {
     // })
     setEnteredDate(e.target.value);
  };
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    const expenseData = {
+      title : enteredTitle,
+      amount : +enteredAmount,
+      date : new Date(enteredDate)
+    }
+    props.onSaveExpenseData(expenseData);
+    setEnteredAmount(''); 
+    setEnteredDate('');
+    setEnteredTitle('');
+  };
+  const cancelHandler = () =>{
+    props.onStopEdit();
+  };
   return(
-    <form>
+    <form onSubmit={submitHandler}>
         <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type="text" onChange={titleChangeHandler}/>
+                {/* // 양방향 바인딩 : 상태를 업데이트하기 위해 입력에서 변경사항 수신하고 입력에 상태를 다시 보내줌 */}
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler}/>
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type="number" onChange={amountChangeHandler} min='0.01' step='0.01'/>
+                <input type="number" value={enteredAmount}onChange={amountChangeHandler} min='0.01' step='0.01'/>
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type="date" onChange={dateChangeHandler} min='2019-01-01' max='2022-12-31'/>
+                <input type="date" value={enteredDate}onChange={dateChangeHandler} min='2019-01-01' max='2022-12-31'/>
             </div>
         </div>
         <div className='new-expense__actions'>
+            <button type="button" onClick={cancelHandler}>Cancel</button>
             <button type="submit">Add Expense</button>
         </div>
     </form>
--- my-react-app/src/components/NewExpense/NewExpense.js
+++ my-react-app/src/components/NewExpense/NewExpense.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useState} from 'react';
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 /**
@@ -6,9 +6,26 @@ import ExpenseForm from './ExpenseForm';
 * @function NewExpense
 **/
 const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    };
+    props.onAddExpense(expenseData);
+    setIsEditing(false);
+  }
+  const [isEditing,setIsEditing] = useState(false);
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  };
   return(
     <div className="new-expense">
-        <ExpenseForm />
+      {!isEditing && <button onClick ={startEditingHandler}>Add new Expense</button>}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onStopEdit={stopEditingHandler}  /> }   {/* -> //포인터 전달(자식 -> 부모로 데이터를 넘기기 위해 / 반대는 props로) */}
     </div>
    )
   }
